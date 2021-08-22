9aaf90623fff2d55a669e76b56b0812cf7cab626
HC <1783834865@qq.com>
2021-08-23 02:30:57 +0800

    已经打卡成功

--- pycharm/main.py
+++ pycharm/main.py
@@ -17,46 +17,50 @@ def gethasFlag(input):
     temp = re.findall(r"hasFlag: '([\s|\S]*)',[\s]*setting", input)
     return temp
 
-def valid(info):
-    if info['sfjcbh']==0:
+def valid(info,sfzgn):
+    if info['sfjcbh']=='0':
         info['jcbhlx']=''
         info['jcbhrq']=''
 
-    if info['sfcyglq']==0:
+    if info['sfcyglq']=='0':
         info['gllx'] = ''
         info['glksrq'] = ''
 
-    if info['sfcxtz'] == 0:
-        info['sfyyjc'] = 0
+    if info['sfcxtz'] == '0':
+        info['sfyyjc'] = '0'
 
-    if info['sfyyjc'] == 0:
+    if info['sfyyjc'] == '0':
         info['jcjgqr'] = 0
         info['jcjg'] = ''
 
-    if info['sfcxzysx'] == 0:
+    if info['sfcxzysx'] == '0':
         info['qksm'] = ''
 
-    if info['sfzgn'] == 1:
+    if sfzgn == 1:
         info['szcs'] = ''
         info['szgj'] = ''
 
-    if 'sfjxhsjc' not in info.keys() or ('sfjxhsjc' in info.keys() and info['sfjxhsjc'] != 1)  :
+    if 'sfjxhsjc' not in info.keys() or ('sfjxhsjc' in info.keys() and info['sfjxhsjc'] != '1')  :
         info['hsjcrq'] = ''
         info['hsjcdd'] = ''
         info['hsjcjg'] = 0
 
-    if info['sfzx'] != 1:
+    if info['sfzx'] != '1':
         info['szxqmc'] = ''
     else:
         info['bzxyy'] = ''
 
-    if info['sfjzdezxgym'] != 1:
+    if info['sfjzdezxgym'] != '1':
         info['jzdezxgymrq'] = ''
 
-    if info['sfjzxgym'] != 1:
+    if info['sfjzxgym'] != '1':
         info['jzxgymrq'] = ''
 
-
+def construct_GeoInfo(oldInfo,adress):
+    Geo_Info={}
+    Geo_Info['formatted_address']=adress
+    Geo_Info['addressComponent']=oldInfo['addressComponent']
+    return  Geo_Info
 
 if __name__ == '__main__':
     loginheader={
@@ -73,44 +77,57 @@ if __name__ == '__main__':
         'Hm_lvt_48b682d4885d22a90111e46b972e3268' : '1629463788, 1629477294',
         'Hm_lpvt_48b682d4885d22a90111e46b972e3268' : '1629477478'
     }
+
+
     geturl='https://wfw.scu.edu.cn/ncov/wap/default/index'
-    r=requests.get(geturl,headers=loginheader,cookies=getcookies);
+    r=requests.get(geturl,headers=loginheader,cookies=getcookies)
+    DEBUG=0
     if r.status_code == requests.codes.ok:
         hasFlag=gethasFlag(r.text)[0]
         if hasFlag=='1':
             print('\n已经完成过打卡，每天只能打卡一次\n')
         else:
-            # print("\nget result is \n"+r.text)
-            #getOldinfo
             oldInfo=getOldinfo(r.text)
             oldInfo=json.loads(oldInfo[0])
-            print("\n oldinfo is :\n")
-            print(oldInfo)
-
+            if DEBUG:
+                print("\n oldinfo is :\n")
+                print(oldInfo)
             defInfo=getDef(r.text)
             defInfo=json.loads(defInfo[0])
-            print("\n definfo is :\n")
-            print(defInfo)
-
+            if DEBUG:
+                print("\n definfo is :\n")
+                print(defInfo)
             #构成打卡信息
             uploadinfo = defInfo
             uploadinfo['szgjcs']= ''
-            uploadinfo['sfzgn']=1
-            print("\n uploadinfo is :\n")
-            print(uploadinfo)
-
-            uploadinfo['geo_api_info']=oldInfo['geo_api_info']
-            oldGeoInfo=json.loads(oldInfo['geo_api_info'])
+            uploadinfo['ismoved']='0'
+            oldGeoInfo = json.loads(oldInfo['geo_api_info'])
+            uploadinfo['geo_api_info'] = oldInfo['geo_api_info']
             uploadinfo['address']=oldGeoInfo['formattedAddress']
             uploadinfo['province']=oldGeoInfo['addressComponent']['province']
             uploadinfo['city']=oldGeoInfo['addressComponent']['city']
             uploadinfo['area']=uploadinfo['province']+' '+uploadinfo['city']+' '+oldGeoInfo['addressComponent']['district']
+            if DEBUG:
+                print("\n uploadinfo is :\n")
+                print(uploadinfo)
 
-            valid(uploadinfo)
+            sfzgn=1
+            valid(uploadinfo,sfzgn)
             uploadurl='https://wfw.scu.edu.cn/ncov/wap/default/save'
-            r=requests.post(uploadurl,json=json.dumps(uploadinfo),cookies=getcookies)
+            json_data=json.dumps(uploadinfo)
+
+            if DEBUG:
+                print('\njson data is\n')
+                print(json_data)
+
+            r=requests.post(uploadurl,data=uploadinfo,cookies=getcookies)
             if r.status_code==requests.codes.ok:
-                print('\n打卡成功！\n')
+                # print(r.text)
+                try:
+                    print('\n打卡结果是\n')
+                    print(r.json()['m'])
+                except ValueError:
+                    print('\n服务器返回结果异常\n')
             else:
                 print('\n打卡失败！\n')
                 print(r.status_code)
